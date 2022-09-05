d71bcccb629042acbe2c39dae1831e02d9401098
seaboyz <seaboyz@hotmail.com>
2022-09-05 10:37:29 -0400

    Add ex18.py

---
+++ ex18.py
@@ -0,0 +1,31 @@
+# this one is like your scripts with argv
+from cgi import print_environ
+
+
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# ok, that *args is actually pointless, we can just do this
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# this juest takes one argument
+
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# this one takes no arguments
+
+
+def print_none():
+    print("I got nothin'.")
+
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
